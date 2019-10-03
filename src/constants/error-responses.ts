faff2c53853c303f163f1f97bff58d450cd5a570
Eric Bratter <eric.bratter@sap.com>
2019-10-03 14:38:26 -0400

    add schema vaildations for profile

--- src/constants/error-responses.ts
+++ src/constants/error-responses.ts
@@ -3,19 +3,30 @@
 const duplicateProfile = {
   code: 'PROFILE_ALREADY_EXISTS',
   message: 'A profile for the provided ID already exists.',
-  technicalError: ''
+  technicalError: '' as any
 };
 
 const profiledIdMissing = {
   code: 'REQUEST_MALFORMED',
   message: 'profileId is a required query parameter',
-  technicalError: ''
+  technicalError: '' as any
+};
+
+const failedSchemaValidation = {
+  code: 'REQUEST_MALFORMED',
+  message: 'Payload does not pass schema validation',
+  technicalError: '' as any
 };
 
 const systemError = {
   code: 'SYSTEM_ERROR',
   message: 'Oops something went wrong on our end.',
-  technicalError: ''
+  technicalError: '' as any
 };
 
-export { duplicateProfile, profiledIdMissing, systemError };
+export {
+  duplicateProfile,
+  profiledIdMissing,
+  failedSchemaValidation,
+  systemError
+};
--- src/handlers/create-profile.ts
+++ src/handlers/create-profile.ts
@@ -1,12 +1,28 @@
 'use strict';
 
+import Ajv from 'ajv';
 import { BAD_REQUEST, CREATED } from 'http-status-codes';
-import { duplicateProfile } from '../constants/error-responses';
+import { clone } from 'lodash';
+import schema from '../../schema/profile.json';
+import {
+  duplicateProfile,
+  failedSchemaValidation
+} from '../constants/error-responses';
 import { IProfile, IProfileRepository } from '../repository/profile-repository';
 
 function createProfileHandlerFactory(profileRepository: IProfileRepository) {
   const createProfile = async (req: any, res: any) => {
     const profile: IProfile = req.body;
+
+    const validator = new Ajv();
+
+    if (!validator.validate(schema, profile)) {
+      res.status(BAD_REQUEST);
+
+      const response = clone(failedSchemaValidation);
+      response.technicalError = validator.errors;
+      res.send(response);
+    }
 
     // TODO: Verify that the authenticated use is the user for the profiled profile.uid
     // TODO: Build in data validations probably based on a JSON Schema or the OpenAPI specification

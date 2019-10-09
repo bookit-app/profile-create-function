3349e721a35d3945f77c109676269a9a07dd505c
Eric Bratter <eric.bratter@sap.com>
2019-10-09 16:12:51 -0400

    finalize unit tests

--- src/lib/util/index.js
+++ src/lib/util/index.js
@@ -5,7 +5,7 @@ const { isEmpty } = require('lodash');
 module.exports.extractTraceIdFromHeader = req => {
   const traceHeader = req.header('X-Cloud-Trace-Context');
 
-  if (isEmpty(traceHeader)) return;
+  if (isEmpty(traceHeader)) return '';
 
   const [trace] = traceHeader.split('/');
   return trace;
--- tests/services/delete-profile.spec.js
+++ tests/services/delete-profile.spec.js
@@ -1,74 +1,101 @@
-// 'use strict';
+'use strict';
 
-// const { expect } = require('chai');
-// const { BAD_REQUEST, CREATED } = require('http-status-codes');
-// const { stub } = require('sinon');
-// const createProfileService = require('../../src/services/create-profile/handler');
+const { expect } = require('chai');
+const {
+  BAD_REQUEST,
+  NO_CONTENT,
+  INTERNAL_SERVER_ERROR,
+  NOT_FOUND
+} = require('http-status-codes');
+const { stub } = require('sinon');
+const deleteProfileService = require('../../src/services/delete-profile/handler');
 
-// const req = {
-//   header: stub().returns('TEST/TRACE'),
-//   body: {
-//     address: {
-//       city: 'city',
-//       state: 'NY',
-//       streetAddress: 'a street somewhere',
-//       zip: 12345
-//     },
-//     birthday: '11-13-2019',
-//     email: 'test@test.com',
-//     firstName: 'test-first-name',
-//     gender: 'M',
-//     isProvider: false,
-//     isSocial: true,
-//     lastName: 'test-last-name',
-//     phoneNumber: '123-123-1234',
-//     uid: 'TEST'
-//   }
-// };
+const profile = {
+  address: {
+    city: 'city',
+    state: 'NY',
+    streetAddress: 'a street somewhere',
+    zip: 12345
+  },
+  birthday: '11-13-2019',
+  email: 'test@test.com',
+  firstName: 'test-first-name',
+  gender: 'M',
+  isProvider: false,
+  isSocial: true,
+  lastName: 'test-last-name',
+  phoneNumber: '123-123-1234',
+  uid: 'TEST'
+};
 
-// const res = {
-//   send: stub(),
-//   sendStatus: stub(),
-//   status: stub()
-// };
+const req = {
+  header: stub().returns('TEST/TRACE'),
+  params: {
+    profileId: 'TEST'
+  }
+};
 
-// const profileRepositoryMock = {
-//   create: stub()
-// };
+const res = {
+  send: stub(),
+  sendStatus: stub(),
+  status: stub()
+};
 
-// const handler = createProfileService(profileRepositoryMock);
+const profileRepositoryMock = {
+  findByProfileId: stub(),
+  delete: stub()
+};
 
-// describe('create-profile: unit tests', () => {
-//   it('should throw error when required dependencies are not provided', () => {
-//     expect(createProfileService).to.throw(Error);
-//   });
+const handler = deleteProfileService(profileRepositoryMock);
 
-//   it('should respond with a 201 when profile is created', () => {
-//     profileRepositoryMock.create.resolves();
-//     expect(handler.createProfile(req, res)).to.be.fulfilled.then(() => {
-//       expect(res.sendStatus.called).to.be.true;
-//       expect(res.sendStatus.calledWithExactly(CREATED)).to.be.true;
-//     });
-//   });
+describe('delete-profile: unit tests', () => {
+  afterEach(() => {
+    profileRepositoryMock.delete.resetHistory();
+    profileRepositoryMock.findByProfileId.resetHistory();
+  });
 
-//   it('should respond with a 400 when the schema validation fails', () => {
-//     profileRepositoryMock.create.resolves();
-//     const badReq = {
-//       header: stub().returns('TEST/TRACE'),
-//       body: {}
-//     };
-//     expect(handler.createProfile(badReq, res)).to.be.fulfilled.then(() => {
-//       expect(res.status.called).to.be.true;
-//       expect(res.status.calledWithExactly(BAD_REQUEST)).to.be.true;
-//     });
-//   });
+  it('should throw error when required dependencies are not provided', () => {
+    expect(deleteProfileService).to.throw(Error);
+  });
 
-//   it('should respond with a 400 when the profile creation fails', () => {
-//     profileRepositoryMock.create.rejects(new Error('FORCED ERROR'));
-//     expect(handler.createProfile(req, res)).to.be.fulfilled.then(() => {
-//       expect(res.status.called).to.be.true;
-//       expect(res.status.calledWithExactly(BAD_REQUEST)).to.be.true;
-//       expect(res.send.called).to.be.true;
-//     });
-//   });
-// });
+  it('should respond with a 204 when profile is deleted', () => {
+    profileRepositoryMock.delete.resolves();
+    profileRepositoryMock.findByProfileId.resolves(profile);
+    expect(handler.deleteProfile(req, res)).to.be.fulfilled.then(() => {
+      expect(res.sendStatus.called).to.be.true;
+      expect(res.sendStatus.calledWithExactly(NO_CONTENT)).to.be.true;
+    });
+  });
+
+  it('should respond with a 400 when the schema validation fails', () => {
+    profileRepositoryMock.delete.resolves();
+    const badReq = {
+      header: stub().returns('TEST/TRACE'),
+      params: {}
+    };
+
+    expect(handler.deleteProfile(badReq, res)).to.be.fulfilled.then(() => {
+      expect(res.status.called).to.be.true;
+      expect(res.status.calledWithExactly(BAD_REQUEST)).to.be.true;
+    });
+  });
+
+  it('should respond with a 404 when the profile does not exist', () => {
+    profileRepositoryMock.findByProfileId.resolves(undefined);
+    profileRepositoryMock.delete.rejects(new Error('FORCED ERROR'));
+    expect(handler.deleteProfile(req, res)).to.be.fulfilled.then(() => {
+      expect(res.sendStatus.called).to.be.true;
+      expect(res.sendStatus.calledWithExactly(NOT_FOUND)).to.be.true;
+    });
+  });
+
+  it('should respond with a 500 when the profile delete fails', () => {
+    profileRepositoryMock.findByProfileId.resolves(profile);
+    profileRepositoryMock.delete.rejects(new Error('FORCED ERROR'));
+    expect(handler.deleteProfile(req, res)).to.be.fulfilled.then(() => {
+      expect(res.status.called).to.be.true;
+      expect(res.status.calledWithExactly(INTERNAL_SERVER_ERROR)).to.be.true;
+      expect(res.send.called).to.be.true;
+    });
+  });
+});
